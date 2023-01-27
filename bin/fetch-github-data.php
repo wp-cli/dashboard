bd799fbf38ab6328490fdfe33e9fc39e0feefd0c
Daniel Bachhuber <daniel.bachhuber@automattic.com>
2023-01-27 11:29:26 -0800

    Add a command to capture contributor activity on a regular basis (#4)
    * Update `--only-data` to support `--only=<category>`
    
    * Add a command to fetch contributor events on a regular basis

--- bin/fetch-github-data.php
+++ bin/fetch-github-data.php
@@ -7,8 +7,14 @@ require_once dirname( __DIR__ ) . '/theme/functions.php';
  *
  * ## OPTIONS
  *
- * [--only-data]
- * : Only update the data points, not the repositories.
+ * [--only=<section>]
+ * : Only rebuild a specific category of data.
+ * ---
+ * options:
+ *   - data
+ *   - contributors
+ *   - repositories
+ * ---
  *
  * [--force]
  * : Forcefully overwrite any existing data.
@@ -41,45 +47,106 @@ function wp_cli_dashboard_fetch_github_data( $args, $assoc_args ) {
 		'Authorization' => 'token ' . getenv( 'GITHUB_TOKEN' ),
 	);
 
-	WP_CLI::log( sprintf( 'Fetching %d GitHub data points...', count( $config['github_data'] ) ) );
-	foreach ( $config['github_data'] as $key => $meta ) {
+	if ( empty( $assoc_args['only'] ) || 'data' === $assoc_args['only'] ) {
+		WP_CLI::log( sprintf( 'Fetching %d GitHub data points...', count( $config['github_data'] ) ) );
+		foreach ( $config['github_data'] as $key => $meta ) {
 
-		if ( empty( $meta['search'] ) ) {
-			WP_CLI::warning( sprintf( 'Invalid \'search\' for %s', $key ) );
-			continue;
-		}
+			if ( empty( $meta['search'] ) ) {
+				WP_CLI::warning( sprintf( 'Invalid \'search\' for %s', $key ) );
+				continue;
+			}
 
-		$time = date( 'Y-m-d-H-00' );
-		$path = WP_CLI_DASHBOARD_BASE_DIR . '/github-data/' . $key . '/' . $time;
-		if ( file_exists( $path ) && empty( $assoc_args['force'] ) ) {
-			WP_CLI::log( sprintf( 'Skipping: Data already exists for %s on %s', $key, $time ) );
-			continue;
-		}
+			$time = date( 'Y-m-d-H-00' );
+			$path = WP_CLI_DASHBOARD_BASE_DIR . '/github-data/' . $key . '/' . $time;
+			if ( file_exists( $path ) && empty( $assoc_args['force'] ) ) {
+				WP_CLI::log( sprintf( 'Skipping: Data already exists for %s on %s', $key, $time ) );
+				continue;
+			}
 
-		$query = array(
-			'q' => $meta['search'],
-		);
-		$response = WP_CLI\Utils\http_request( 'GET', 'https://api.github.com/search/issues', $query, $headers );
-		if ( 20 !== (int) substr( $response->status_code, 0, 2 ) ) {
-			WP_CLI::error(
-				sprintf(
-					"Failed request. GitHub API returned: %s (HTTP code %d)",
-					$response->body,
-					$response->status_code
-				)
+			$query = array(
+				'q' => $meta['search'],
 			);
-		}
-		$data = json_decode( $response->body );
-		$total_count = $data->total_count;
+			$response = WP_CLI\Utils\http_request( 'GET', 'https://api.github.com/search/issues', $query, $headers );
+			if ( 20 !== (int) substr( $response->status_code, 0, 2 ) ) {
+				WP_CLI::error(
+					sprintf(
+						"Failed request. GitHub API returned: %s (HTTP code %d)",
+						$response->body,
+						$response->status_code
+					)
+				);
+			}
+			$data = json_decode( $response->body );
+			$total_count = $data->total_count;
 
-		if ( ! is_dir( dirname( $path ) ) ) {
-			mkdir( dirname( $path ), 0777, true );
+			if ( ! is_dir( dirname( $path ) ) ) {
+				mkdir( dirname( $path ), 0777, true );
+			}
+			file_put_contents( $path, $total_count );
+			WP_CLI::log( sprintf( 'Saved: Total count for %s on %s: %d', $key, $time, $total_count ) );
 		}
-		file_put_contents( $path, $total_count );
-		WP_CLI::log( sprintf( 'Saved: Total count for %s on %s: %d', $key, $time, $total_count ) );
 	}
 
-	if ( ! WP_CLI\Utils\get_flag_value( $assoc_args, 'only-data' ) ) {
+	if ( empty( $assoc_args['only'] ) || 'contributors' === $assoc_args['only'] ) {
+		WP_CLI::log( sprintf( 'Fetching GitHub contributor data for %d repositories...', count( $config['github_repositories'] ) ) );
+		foreach ( $config['github_repositories'] as $repo ) {
+
+			$actors           = array();
+			$repo_short       = str_replace( 'wp-cli/', '', $repo );
+			$most_recent_date = strtotime( '2 weeks ago' );
+			$page             = 1;
+			do {
+				$query    = array(
+					'page'     => $page,
+					'per_page' => 100,
+				);
+				$response = WP_CLI\Utils\http_request( 'GET', sprintf( 'https://api.github.com/repos/%s/issues/events', $repo ), $query, $headers );
+				if ( 20 !== (int) substr( $response->status_code, 0, 2 ) ) {
+					WP_CLI::error(
+						sprintf(
+							"Failed request. GitHub API returned: %s (HTTP code %d)",
+							$response->body,
+							$response->status_code
+						)
+					);
+				}
+				$data = json_decode( $response->body );
+				foreach ( $data as $event ) {
+					if ( false !== stripos( $event->actor->login, '[bot]' ) ) {
+						continue;
+					}
+					if ( ! isset( $actors[ $event->actor->login ] ) ) {
+						$actors[ $event->actor->login ] = array();
+					}
+					$event_time                       = strtotime( $event->created_at );
+					$actors[ $event->actor->login ][] = gmdate( 'Y-m-d', $event_time );
+				}
+				$page++;
+			} while ( $most_recent_date < $event_time );
+
+			foreach ( $actors as $login => $dates ) {
+				$path     = WP_CLI_DASHBOARD_BASE_DIR . '/github-data/contributors/' . $login;
+				$existing = array();
+				if ( file_exists( $path ) ) {
+					$existing = explode( PHP_EOL, file_get_contents( $path ) );
+				}
+				$dates = array_merge( $existing, $dates );
+				rsort( $dates );
+				$dates = array_unique( $dates );
+				if ( ! is_dir( dirname( $path ) ) ) {
+					mkdir( dirname( $path ), 0777, true );
+				}
+				file_put_contents( $path, implode( PHP_EOL, $dates ) );
+			}
+
+			if ( ! is_dir( dirname( $path ) ) ) {
+				mkdir( dirname( $path ), 0777, true );
+			}
+
+		}
+	}
+
+	if ( empty( $assoc_args['only'] ) || 'repositories' === $assoc_args['only'] ) {
 		WP_CLI::log( sprintf( 'Fetching %d GitHub repository data...', count( $config['github_repositories'] ) ) );
 		foreach ( $config['github_repositories'] as $repo ) {
 
